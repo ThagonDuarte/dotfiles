917f73c9f2379d7f6d677cb0a469aeadc4df2050
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-08-31 19:02:57 +0200

    fixed absolute home path

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
 function config --wraps='/usr/bin/git --git-dir=/home/alex/.dotfiles/ --work-tree=/home/alex' --description 'alias config /usr/bin/git --git-dir=/home/alex/.dotfiles/ --work-tree=/home/alex'
-  /usr/bin/git --git-dir=/home/alex/.dotfiles/ --work-tree=/home/alex $argv; 
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv; 
 end
