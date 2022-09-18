71a0fffd5a2a23d871964e64c8f9b4813b3bd2d3
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-09-18 13:10:10 +0200

    started on system install script

---
+++ installDots.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Install pacman packages
+sudo pacman -S $(cat ~/pacman-list)
+
+# Install paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git /tmp/paru
+cd /tmp/paru
+makepkg -si
+cd ~
+
+# Install paru packages
+paru -S $(cat ~/paru-list)
+
+# Link all scripts to .local/bin
+chmod +x ~/.scripts/* &
+~/.scripts/link.sh &
+
+# Link Nextcloud Pictures to home
+rm ~/Pictures &
+ln -s ~/Nextcloud/Media/Pictures ~ &
+
