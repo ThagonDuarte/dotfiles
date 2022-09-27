183323d5dc4eafa4e2f6df5d37afda7fdc0e09b5
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-09-27 15:24:28 +0200

    added picom

---
+++ .scripts/togglepicom.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/picom"
+
+if pgrep -x picom; then
+  # Terminate already running bar instances
+  killall -q picom 
+else
+  # Launch picom
+  picom &
+fi
+
