0c1d51af125435236c31a40bd6cf1a23783b3696
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-10-17 14:03:14 +0200

    updated polybar for seperate laptop and pc configs

--- .scripts/link.sh
+++ .scripts/link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scripts=("apps" "bar" "brightness" "fetch" "launcher" "powermenu" "quicklinks" "screenshot" "term" "wifimenu" "togglepicom")
+scripts=("apps" "bar-pc" "bar-laptop" "brightness" "fetch" "launcher" "powermenu" "quicklinks" "screenshot" "term" "wifimenu" "togglepicom")
 
 for script in "${scripts[@]}"; do
   sudo rm /usr/bin/$script 
