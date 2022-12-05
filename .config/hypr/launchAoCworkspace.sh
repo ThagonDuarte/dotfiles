ffc54b695746d857c71a43c7b36f6f81efe88efa
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-12-05 09:25:42 +0100

    updated hyprland config and keybinds

---
+++ .config/hypr/launchAoCworkspace.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+hyprctl dispatch -- workspace empty
+hyprctl dispatch -- exec firefox --new-window https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html
+sleep 1
+hyprctl dispatch -- exec firefox --new-tab https://doc.rust-lang.org/std/index.html
+hyprctl dispatch -- exec firefox --new-window https://adventofcode.com/2022
+sleep 7 
+hyprctl dispatch -- exec kitty nvim ~/Projects/AoC2022
