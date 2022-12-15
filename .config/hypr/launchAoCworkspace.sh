29fb2e96b46ddadb38dea1778e44604a573abf1e
Alexander Schmidt <alexander.schmidt@tuhh.de>
2022-12-15 08:51:38 +0100

    updated hypr files

--- .config/hypr/launchAoCworkspace.sh
+++ .config/hypr/launchAoCworkspace.sh
@@ -3,6 +3,6 @@ hyprctl dispatch -- workspace empty
 hyprctl dispatch -- exec firefox --new-window https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html
 sleep 1
 hyprctl dispatch -- exec firefox --new-tab https://doc.rust-lang.org/std/index.html
-hyprctl dispatch -- exec firefox --new-window https://adventofcode.com/2022
+hyprctl dispatch -- exec firefox --new-tab https://adventofcode.com/2022
 sleep 7 
 hyprctl dispatch -- exec kitty nvim ~/Projects/AoC2022
