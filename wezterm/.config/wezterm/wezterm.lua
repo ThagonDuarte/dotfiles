209a0d5f42e12b212bed6386d2b0889a7e83cdd1
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-06-15 10:51:21 +0200

    Add initial wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.color_scheme = 'GruvboxDark'
+config.enable_tab_bar = false
+
+return config
+
